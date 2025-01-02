e8a5297fbff6ceac05d6f6333d4034a468ec68c1
Pedro Rigo <pedrorigooliveira@hotmail.com>
2025-01-02 17:53:48 -0300

    Adicionando novas rotas na API

--- src/controllers/teamController.js
+++ src/controllers/teamController.js
@@ -9,8 +9,29 @@ const saveToDatabase = (data) => {
 };
 
 const loadDatabase = () => {
-  if (!fs.existsSync(DB_PATH)) return { teams: [] };
+  if (!fs.existsSync(DB_PATH)) return { teams: [], pointsTable: [] };
   return JSON.parse(fs.readFileSync(DB_PATH));
+};
+
+const createPointsEntry = (team) => {
+  return {
+    id: team.id,
+    name: team.name,
+    stats: {
+      points: 0,
+      wins: 0,
+      draws: 0,
+      losses: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalDifference: 0,
+      matchesPlayed: 0,
+      winPercentage: 0,
+      yellowCards: 0,
+      redCards: 0,
+    },
+    matchHistory: [],
+  };
 };
 
 const createTeam = (req, res) => {
@@ -24,6 +45,7 @@ const createTeam = (req, res) => {
   const database = loadDatabase();
 
   database.teams.push(newTeam);
+  database.pointsTable.push(createPointsEntry(newTeam));
   saveToDatabase(database);
 
   res.status(201).send({ message: 'Time criado com sucesso!', team: newTeam });
@@ -45,7 +67,15 @@ const updateTeam = (req, res) => {
     return res.status(404).send({ error: 'Time não encontrado!' });
   }
 
-  if (name) database.teams[teamIndex].name = name;
+  if (name) {
+    database.teams[teamIndex].name = name;
+
+    const pointsIndex = database.pointsTable.findIndex((entry) => entry.id === parseInt(id));
+    if (pointsIndex !== -1) {
+      database.pointsTable[pointsIndex].name = name;
+    }
+  }
+
   if (players && players.length === 11) {
     database.teams[teamIndex].players = players;
   } else if (players) {
@@ -67,10 +97,84 @@ const deleteTeam = (req, res) => {
     return res.status(404).send({ error: 'Time não encontrado!' });
   }
 
-  const deletedTeam = database.teams.splice(teamIndex, 1);
+  database.teams.splice(teamIndex, 1);
+  database.pointsTable = database.pointsTable.filter((entry) => entry.id !== parseInt(id));
   saveToDatabase(database);
 
-  res.send({ message: 'Time excluído com sucesso!', team: deletedTeam[0] });
+  res.send({ message: 'Time excluído com sucesso!' });
 };
 
-module.exports = { createTeam, getTeams, updateTeam, deleteTeam };
+const getPointsTable = (req, res) => {
+  const database = loadDatabase();
+  res.send(database.pointsTable);
+};
+
+const updateStatistics = (game, database) => {
+  const { homeTeam, awayTeam, homeGoals, awayGoals } = game;
+
+  const homeTeamStats = database.pointsTable.find(team => team.id === homeTeam.id);
+  const awayTeamStats = database.pointsTable.find(team => team.id === awayTeam.id);
+
+  if (!homeTeamStats || !awayTeamStats) {
+    throw new Error("Um ou ambos os times não existem na tabela de pontos.");
+  }
+
+  homeTeamStats.goalsFor += homeGoals;
+  homeTeamStats.goalsAgainst += awayGoals;
+  homeTeamStats.goalDifference = homeTeamStats.goalsFor - homeTeamStats.goalsAgainst;
+
+  awayTeamStats.goalsFor += awayGoals;
+  awayTeamStats.goalsAgainst += homeGoals;
+  awayTeamStats.goalDifference = awayTeamStats.goalsFor - awayTeamStats.goalsAgainst;
+
+  if (homeGoals > awayGoals) {
+    homeTeamStats.wins += 1;
+    homeTeamStats.points += 3;
+    awayTeamStats.losses += 1;
+  } else if (homeGoals < awayGoals) {
+    awayTeamStats.wins += 1;
+    awayTeamStats.points += 3;
+    homeTeamStats.losses += 1;
+  } else {
+    homeTeamStats.draws += 1;
+    homeTeamStats.points += 1;
+    awayTeamStats.draws += 1;
+    awayTeamStats.points += 1;
+  }
+};
+
+const registerGame = (req, res) => {
+  const { homeTeamId, awayTeamId, homeGoals, awayGoals, date } = req.body;
+
+  const database = loadDatabase();
+
+  const homeTeam = database.teams.find(team => team.id === homeTeamId);
+  const awayTeam = database.teams.find(team => team.id === awayTeamId);
+
+  if (!homeTeam || !awayTeam) {
+    return res.status(404).send({ error: "Um ou ambos os times não existem!" });
+  }
+
+  const newGame = {
+    id: database.games.length + 1,
+    date,
+    homeTeam: { id: homeTeam.id, name: homeTeam.name },
+    awayTeam: { id: awayTeam.id, name: awayTeam.name },
+    homeGoals,
+    awayGoals,
+  };
+
+  database.games.push(newGame);
+
+  try {
+    updateStatistics(newGame, database);
+    saveToDatabase(database);
+
+    res.status(201).send({ message: "Jogo registrado e estatísticas atualizadas!", game: newGame });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+
+module.exports = { createTeam, getTeams, updateTeam, deleteTeam, getPointsTable, registerGame, updateStatistics };
--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createTeam, getTeams, updateTeam, deleteTeam } = require('../controllers/teamController');
+const { createTeam, getTeams, updateTeam, deleteTeam, getPointsTable, registerGame } = require('../controllers/teamController');
 
 router.post('/', createTeam);
 router.get('/', getTeams);
 router.put('/:id', updateTeam);
-router.delete('/', deleteTeam);
+router.delete('/:id', deleteTeam);
+router.get('/points-table', getPointsTable);
+router.post('/games', registerGame);
 
 module.exports = router;
